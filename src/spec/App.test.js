b796b28974ff325bb21fc7fe5eab8e0cb9adada0
Patrick Mackridge <pat.mack@talktalk.net>
2020-11-07 13:05:47 +0000

    UI tests for header and search fields

--- src/spec/App.test.js
+++ src/spec/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the title of the page', () => {
+  const { getByText } = render(<App />);
+  const title = getByText('Song Rater');
+  expect(title).toBeInTheDocument();
+});
+
+test('renders the choose artist and song title inputs', () => {
+  const { getByText } = render(<App />);
+  const artistSearch = getByText('Artist:');
+  const songSearch = getByText('Song:');
+  expect(artistSearch).toBeInTheDocument();
+  expect(songSearch).toBeInTheDocument();
 });
