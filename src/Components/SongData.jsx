4e3486fadbb2dab07c8274e09fd3e8377c870dea
Patrick Mackridge <pat.mack@talktalk.net>
2020-11-08 14:55:11 +0000

    Users can rate songs once they are fetched from the API

--- src/Components/SongData.jsx
+++ src/Components/SongData.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../styles/App.css';
 
 function SongData(props) {
@@ -7,6 +8,8 @@ function SongData(props) {
   const isRealSong = () => {
     return songName && artistName && albumName ? true : false;
   }
+
+  const [rating, setRating] = useState('')
   
   return (
     <>
@@ -15,7 +18,25 @@ function SongData(props) {
       <p>{songName}</p>
       <p>By: {artistName}</p>
       <p>From the album: {albumName}</p>
-    <img src={albumImage} alt="Album artwork"/> </> : <p>No song found, please check your song and artist name to try again.</p>}
+      <img src={albumImage} alt="Album artwork"/> 
+      <div className="songRater">
+        <label className="ratingLabel">Rate this song:
+          <select className="songRating" onChange={event => setRating(event.target.value)}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </label>
+        <button className="saveRating" disabled={!rating}>Save Rating</button>
+      </div>
+    </> : <p>No song found, please check your song and artist name to try again.</p>}
     </>
   );
 }
